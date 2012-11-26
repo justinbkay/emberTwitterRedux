8f57c786686fa2dad4aa686c80ed34b473389f0a
Justin B Kay <jkay@nwrecc.org>
2012-11-26 16:51:48 -0700

    Adding the baseline ember code to app.js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+App = Ember.Application.create();
+
+App.ApplicationView = Ember.View.extend({
+  templateName: 'application'
+});
+App.ApplicationController = Ember.Controller.extend();
+
+
+App.Router = Ember.Router.extend({
+  root: Ember.Route.extend({
+    index: Ember.Route.extend({
+      route: '/'
+    })
+  })
+});
+
+App.initialize();
