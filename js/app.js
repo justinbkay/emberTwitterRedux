3bc342df11db1fec580d9e3ad8c1bcd90f04da49
Justin B Kay <jkay@nwrecc.org>
2012-11-27 15:27:22 -0700

    loading state added
    Moved the instructions state to a child state of index.  Added the
    loading state as a child state of index.  Triggering the show loading
    state when the insertNewline event is received by the view class.

--- js/app.js
+++ js/app.js
@@ -7,13 +7,17 @@ App.ApplicationController = Ember.Controller.extend();
 
 App.SearchTextField = Ember.TextField.extend({
   insertNewline: function() {
-    //App.tweetsC.loadTweets();
+    this.get('controller.target').send('showLoading');
   }
 });
 
 //App.InstructionsController = Ember.Controller.extend();
 App.InstructionsView = Ember.View.extend({
   templateName: 'instructions'
+});
+
+App.LoadingView = Ember.View.extend({
+  templateName: 'loading'
 });
 
 App.TweetsController = Ember.ArrayController.extend();
@@ -25,9 +29,27 @@ App.Router = Ember.Router.extend({
   root: Ember.Route.extend({
     index: Ember.Route.extend({
       route: '/',
-      connectOutlets: function(router){
-        router.get('applicationController').connectOutlet('instructions');
-      }
+      showLoading: Ember.Route.transitionTo('loading'),
+      //connectOutlets: function(router){
+        //router.get('applicationController').connectOutlet('instructions');
+      //},
+
+      //child states
+      initialState: 'instructions',
+
+      instructions: Ember.Route.extend({
+        route: '/',
+        connectOutlets: function(router) {
+          router.get('applicationController').connectOutlet('instructions');
+        }
+      }),
+
+      loading: Ember.Route.extend({
+        route: '/',
+        connectOutlets: function(router) {
+          router.get('applicationController').connectOutlet('loading');
+        }
+      })
     })
   })
 });
