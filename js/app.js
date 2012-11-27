66c09bceb2837bd188901fde7c751057625ad52b
Justin B Kay <jkay@nwrecc.org>
2012-11-27 09:59:13 -0700

    Enough code to get the search field to load in the initial state
    The initial state shows the instructions to enter a search term into
    the textbox.

--- js/app.js
+++ js/app.js
@@ -5,11 +5,29 @@ App.ApplicationView = Ember.View.extend({
 });
 App.ApplicationController = Ember.Controller.extend();
 
+App.SearchTextField = Ember.TextField.extend({
+  insertNewline: function() {
+    //App.tweetsC.loadTweets();
+  }
+});
+
+//App.InstructionsController = Ember.Controller.extend();
+App.InstructionsView = Ember.View.extend({
+  templateName: 'instructions'
+});
+
+App.TweetsController = Ember.ArrayController.extend();
+App.TweetsView = Ember.View.extend({
+  templateName: 'tweets'
+});
 
 App.Router = Ember.Router.extend({
   root: Ember.Route.extend({
     index: Ember.Route.extend({
-      route: '/'
+      route: '/',
+      connectOutlets: function(router){
+        router.get('applicationController').connectOutlet('instructions');
+      }
     })
   })
 });
