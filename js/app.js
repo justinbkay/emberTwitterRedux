bc096777a77f42ec4008cca7160bcbddd62296b4
Justin B Kay <jkay@nwrecc.org>
2012-11-28 09:11:07 -0700

    Tweets are loading and displaying in the list

--- js/app.js
+++ js/app.js
@@ -8,7 +8,17 @@ App.ApplicationController = Ember.Controller.extend();
 App.SearchTextField = Ember.TextField.extend({
   insertNewline: function() {
     this.get('controller.target').send('showLoading');
+    // load the tweets
+    App.Tweet.loadTweets(this.value);
+    this.get('controller.target').send('showTweets');
   }
+});
+
+App.Tweet = Ember.Object.extend({
+  profilePic: null,
+  username: null,
+  date: null,
+  text: null
 });
 
 //App.InstructionsController = Ember.Controller.extend();
@@ -20,9 +30,56 @@ App.LoadingView = Ember.View.extend({
   templateName: 'loading'
 });
 
-App.TweetsController = Ember.ArrayController.extend();
+App.TweetsController = Ember.ArrayController.extend({
+});
 App.TweetsView = Ember.View.extend({
   templateName: 'tweets'
+});
+
+App.Tweet.reopenClass({
+  loadedTweets: [],
+
+  loadTweets: function(term) {
+    if ( term ) {
+      var url = 'http://search.twitter.com/search.json?q=%@&rpp=15&include_entities=true&result_type=recent&callback=?'.fmt(encodeURIComponent(term));
+      this.loadedTweets = [];
+      $.ajax({
+        url: url,
+        dataType: 'JSON',
+        context: this,
+        success: this.loadTweetsSucceeded,
+        complete: this.loadTweetsCompleted
+      });
+    }
+    return this.loadedTweets;
+  },
+
+  loadTweetsCompleted: function(xhr) {
+    if(xhr.status == 400 || xhr.status == 420) {
+      this.set('limit', true);
+    }
+  },
+
+  loadTweetsSucceeded: function(data) {
+    var tweets = data.results;
+    tweets.forEach(function(tweetData) {
+      var d = new Date(tweetData.created_at);
+      var ago = $.timeago(d);
+      var tweet = App.Tweet.create({
+        profilePic: tweetData.profile_image_url,
+        username: tweetData.from_user,
+        date: ago,
+        text: tweetData.text
+      });
+      this.loadedTweets.addObject(tweet);
+    }, this);
+
+    if (this.loadedTweets.length === 0) {
+      this.set('noresults', true);
+    }
+
+  }
+
 });
 
 App.Router = Ember.Router.extend({
@@ -30,9 +87,7 @@ App.Router = Ember.Router.extend({
     index: Ember.Route.extend({
       route: '/',
       showLoading: Ember.Route.transitionTo('loading'),
-      //connectOutlets: function(router){
-        //router.get('applicationController').connectOutlet('instructions');
-      //},
+      showTweets: Ember.Route.transitionTo('showResults'),
 
       //child states
       initialState: 'instructions',
@@ -49,7 +104,15 @@ App.Router = Ember.Router.extend({
         connectOutlets: function(router) {
           router.get('applicationController').connectOutlet('loading');
         }
+      }),
+
+      showResults: Ember.Route.extend({
+        route: '/',
+        connectOutlets: function(router) {
+          router.get('applicationController').connectOutlet('tweets', App.Tweet.loadedTweets);
+        }
       })
+
     })
   })
 });
